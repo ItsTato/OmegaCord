a5bc5b243b69b5a29d49a2d37b9d5926793e8938
ItsTato <itstato.official@gmail.com>
2024-05-31 21:03:18 -0600

    Remove

--- Servers/OldPsi/omegaservertools/Server.py
+++
@@ -1,6 +0,0 @@
-class Server:
-	def __init__(self,id:int,name:str,icon_url:str,channels:dict,members:list) -> None:
-		self.ID:int = id
-		self.NAME:str = name
-		self.ICON_URL:str = icon_url
-		self.CHANNELS:dict = channels
